013461126f795c6a970d510d21251fe78d58b56b
StevenDelval <asteven59220@free.fr>
2024-01-17 16:45:01 +0100

    Modify app to download pdf

--- app.py
+++ app.py
@@ -4,9 +4,32 @@ import numpy as np
 from PIL import Image
 import pytesseract
 import re
+from PyPDF2 import PdfWriter, PdfReader
+from io import BytesIO
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import A4
 
+buffer = BytesIO()
 # Adding custom options
 custom_config = r'--oem 2 --psm 12 -l fra+eng'
+
+def resize_image(image):
+    """
+    Resize the input image to a fixed size of (1946, 1080).
+
+    Parameters:
+    - image: Input image (OpenCV format)
+
+    Returns:
+    - Resized image
+    """
+    # Resize the image to the fixed size
+    resized_image = cv2.resize(image, (460, 800))
+
+    return resized_image
+
+
+
 
 def biggestRectangle(contours):
     max_area = 0
@@ -22,15 +45,39 @@ def biggestRectangle(contours):
                 indexReturn = index
     return indexReturn
 
-def process_image(img):
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img = cv2.resize(img, None, fx=1.5, fy=1.5, interpolation=cv2.INTER_CUBIC)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+def image_brightness(img):
+    """
+    Process the input image by converting color space, applying gamma correction,
+    and applying binary thresholding.
+
+    Parameters:
+    - img: Input image (OpenCV format)
+
+    Returns:
+    - Processed image
+    """
+    # Convert color space from BGR to RGB
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    # Convert the image to grayscale
+    gray = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2GRAY)
+
+    # Apply gamma correction
     invGamma = 1.0 / 0.3
     table = np.array([((i / 255.0) ** invGamma) * 255 for i in np.arange(0, 256)]).astype("uint8")
     gray = cv2.LUT(gray, table)
-    ret, thresh1 = cv2.threshold(gray, 60, 255, cv2.THRESH_BINARY)
 
+    # Apply binary thresholding
+    _, thresh1 = cv2.threshold(gray, 60, 255, cv2.THRESH_BINARY)
+
+    return thresh1
+
+
+def process_image(img):
+    
+    thresh1 = image_brightness(img)
+    
     contours, hierarchy = cv2.findContours(thresh1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[-2:]
 
     indexReturn = biggestRectangle(contours)
@@ -40,49 +87,47 @@ def process_image(img):
     cv2.drawContours(mask, contours, indexReturn, 255, -1)
     out = np.zeros_like(img)
     out[mask == 255] = img[mask == 255]
+    
 
     (y, x, _) = np.where(mask == 255)
     (topy, topx) = (np.min(y), np.min(x))
     (bottomy, bottomx) = (np.max(y), np.max(x))
     out = img[topy:bottomy + 1, topx:bottomx + 1, :]
+    out = resize_image(out)
+    
 
     return out
 
-def extract_chars(regex, boxes, text):
-    """
-    Extract the first text based on the provided regex and coordinates.
 
-    Parameters:
-    - regex: Regular expression pattern to match.
-    - boxes: The total number of OCR results.
-    - text: The OCR result dictionary containing 'left', 'top', 'width', 'height', and 'text'.
 
-    Returns:
-    - The first text that matches the specified criteria.
-    """
-    following_chars = []
-    liste_d2 = []
-    is_following_d2 = False
-    target_y_d2 = 0  # Set an initial value for target_y
 
-    # Process OCR results
-    for i in range(boxes):
-        (x, y, w, h, chara) = (text['left'][i], text['top'][i], text['width'][i], text['height'][i], text["text"][i])
+# Define a function to trim and save an image
+def trim_and_save(region_key, region_points, image):
+    x, y, w, h = cv2.boundingRect(np.array(region_points))
+    cropped_image = image[y:y+h, x:x+w]
+    cv2.imwrite(f"./img/{region_key}_cropped.jpg", cropped_image)
 
-        # Check if the coordinates match the region of interest ('D.2')
-        if re.search(regex, chara):
-            target_y_d2 = y
-            is_following_d2 = True
-            liste_d2.append((x, y, w, h, chara))
-        elif is_following_d2 and target_y_d2 - 10 <= y <= target_y_d2 + 10:
-            if chara.strip():  # Check if chara is not empty after stripping whitespace
-                following_chars.append(chara)
-                is_following_d2 = False  # Stop capturing characters after the first non-null character
 
-    # Extract the first text from the following_chars list
-    first_text_d2 = following_chars[0] if following_chars else None
 
-    return first_text_d2
+def image2text(cropped_image_path):
+    text = ''
+    
+    cropped_image = cv2.imread(cropped_image_path)
+
+    text = pytesseract.image_to_string(cropped_image, config=custom_config)
+
+    if len(text)!=0:
+        return text
+    else:
+        print("no text detected")
+
+regions_dict = {
+    'points_N_immatriculation':[(31, 35), (31, 46), (100, 49), (101, 35)],
+    'points_titre_document': [(113, 0), (114, 15), (339, 15), (336, 0)],
+    'points_d_2': [(38, 242), (38, 262), (359, 262), (359, 242)],
+    'points_E': [(288, 276), (289, 293), (412, 295), (412, 275)],
+    'points_Date_de_1er_immarticulation': [(145, 37), (146, 50), (228, 51), (226, 36)]
+}
 
 
 def main():
@@ -95,49 +140,65 @@ def main():
 
         img_array = np.array(image)
         processed_img = process_image(img_array)
-
-        st.subheader("Processed Image")
-        st.image(processed_img, caption="Processed Image.", use_column_width=True)
-
-        text = pytesseract.image_to_data(Image.fromarray(processed_img), output_type='dict',config=custom_config)
-        liste_d2 = []
-        # following_chars = []
-        target_y_d2 = None
-        is_following_d2 = False
-        N_immatriculation = None
-        dates = []
-        E_chara = []
+        for region_key, region_points in regions_dict.items():
+            trim_and_save(region_key, region_points, processed_img)
+        dict_var = {}
+        for region_key, value in regions_dict.items():
+            cropped_image_path = f"./img/{region_key}_cropped.jpg"
+            # cropped_image = cv2.imread(cropped_image_path)
+            word_found = image2text(cropped_image_path)
+            dict_var[region_key]=word_found.strip()
         
-        boxes = len(text['level'])
-        # Process OCR results
-        for i in range(boxes):
-            (x, y, w, h, chara) = (text['left'][i], text['top'][i], text['width'][i], text['height'][i], text["text"][i])
-
-            # Check if the coordinates match the region of interest ('N. d'immatriculation')
-            if N_immatriculation is None and re.search(r'[A-Z]{2}-\d{3}-[A-Z]{2}', chara):
-                N_immatriculation = chara
-
-            # Check if the coordinates match the region of interest (dates)
-            if re.search(r'\b\d{2}/\d{2}/\d{4}\b', chara):
-                dates.append(chara)
-
-
-        result_d2 = extract_chars(r'^[dD0]\.2', boxes, text)
-        result_E = extract_chars(r'\bE\b', boxes, text)
-        result_D_3 = extract_chars(r'^[dD0]', boxes, text)
-
-
         # Print the results
-        st.write(f"D.2 : {result_d2}")
-        st.write(f"E. : {result_E}")
-        st.write(f"D.3 : {result_D_3}")
-        st.write(f"N. d'immatriculation  : {N_immatriculation}")
-        st.write(f"Date de 1er immarticulation : {dates[0]}")
-        st.write(f"Visite avant le  : {dates[1]}")
-        st.write(f"Date d'immarticulation actuelle  : {dates[2]}")
-        st.write(f"D.2 : {result_d2[-1]}")
 
-        st.text_input("D2",result_d2,key="result_d2")
+        dict_var['points_N_immatriculation'] = st.text_input("N°immatriculation",dict_var['points_N_immatriculation'],key="'points_N_immatriculation'")
+        dict_var['points_d_2'] = st.text_input("D.2",dict_var['points_d_2'],key="points_d_2")
+        dict_var['points_E'] = st.text_input("E",dict_var['points_E'],key="points_E")
+        dict_var['points_Date_de_1er_immarticulation'] = st.text_input("Date de 1er immarticulation",dict_var['points_Date_de_1er_immarticulation'],key="points_Date_de_1er_immarticulation")
+        button = st.button("Confirmer")
+        if button:
+
+            p = canvas.Canvas(buffer, pagesize=A4)
+            dict_pos = {
+                'points_N_immatriculation':[50, 705],
+                'points_d_2': [50, 630],
+                'points_E': [50, 610],
+                'points_Date_de_1er_immarticulation': [445, 705]
+            }
+            for region_key in dict_pos.keys():
+                if "Date" not in region_key:
+                    p.drawString(*dict_pos[region_key], dict_var[region_key],charSpace=2)
+                else:
+                    p.drawString(*dict_pos[region_key], dict_var[region_key].replace("/",""),charSpace=8.5)
+            p.showPage()
+            p.save()
+
+            #move to the beginning of the StringIO buffer
+            buffer.seek(0)
+            newPdf = PdfReader(buffer)
+
+            # #######DEBUG NEW PDF created#############
+            # pdf1 = buffer.getvalue()
+            # open('pdf1.pdf', 'wb').write(pdf1)
+            #########################################
+            # read your existing PDF
+            existingPdf = PdfReader(open('cerfa_13750-07.pdf', 'rb'))
+            output = PdfWriter()
+            # add the "watermark" (which is the new pdf) on the existing page
+            page = existingPdf.pages[0]
+            page.merge_page(newPdf.pages[0])
+            output.add_page(page)
+            output_bytes = BytesIO()
+            output.write(output_bytes)
+            
+
+            st.download_button(label="Certificat",
+                    data=output_bytes,
+                    file_name="Certificat.pdf")
+            
+
+
+
 
 if __name__ == "__main__":
     main()
